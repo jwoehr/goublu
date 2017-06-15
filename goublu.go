a09ad746a7a47abead2bd633c8e440bd51cb57ac
Jack Woehr <jwoehr@absolute-performance.com>
2017-06-14 21:50:51 -0600

    cleanup

--- goublu.go
+++ goublu.go
@@ -2,6 +2,7 @@
 // goublu launches and serves as a better-than-Java console for
 // https://github.com/jwoehr/ublu Ublu, a Java-coded domain-specific language
 // for remote programming of IBM midrange and mainframe systems.
+// Neither this project nor Ublu are associated with IBM.
 package main
 
 import (
@@ -13,6 +14,8 @@ import (
 	"os"
 	"os/exec"
 )
+
+var DefaultEditor gocui.Editor
 
 // Do layout in cogui loop.
 // Most of the work actually in go functions in main()
@@ -39,54 +42,17 @@ func layout(g *gocui.Gui) error {
 	return nil
 }
 
-/*
-// Output to ublu's output view
-func ubluout(g *gocui.Gui, text string) {
-	// g.Execute(func(g *gocui.Gui) error {
-		v, err := g.View("ubluout")
-		if err != nil {
-			// handle error
-		}
-		fmt.Fprint(v, text)
-	//	return nil
-	// })
-}
-*/
-
 // Exit via the gui instead of via Ublu
 func quit(g *gocui.Gui, v *gocui.View) error {
 	return gocui.ErrQuit
 }
 
-// DefaultEditor is the default editor.
-var DefaultEditor gocui.Editor = gocui.EditorFunc(simpleEditor)
-
-// simpleEditor is used as the default gocui editor.
-func simpleEditor(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
-	switch {
-	case ch != 0 && mod == 0:
-		v.EditWrite(ch)
-	case key == gocui.KeySpace:
-		v.EditWrite(' ')
-	case key == gocui.KeyBackspace || key == gocui.KeyBackspace2:
-		v.EditDelete(true)
-	case key == gocui.KeyDelete:
-		v.EditDelete(false)
-	case key == gocui.KeyInsert:
-		v.Overwrite = !v.Overwrite
-		/*
-			case key == KeyEnter:
-				v.EditNewLine()
-		*/
-	case key == gocui.KeyArrowDown:
-		v.MoveCursor(0, 1, false)
-	case key == gocui.KeyArrowUp:
-		v.MoveCursor(0, -1, false)
-	case key == gocui.KeyArrowLeft:
-		v.MoveCursor(-1, 0, false)
-	case key == gocui.KeyArrowRight:
-		v.MoveCursor(1, 0, false)
+func ubluout(g *gocui.Gui, text string) {
+	v, err := g.View("ubluout")
+	if err != nil {
+		// handle error
 	}
+	fmt.Fprint(v, text)
 }
 
 func main() {
@@ -117,27 +83,16 @@ func main() {
 	// Deliver Ublu's stdout
 	go func() {
 		for {
-
 			text, _ := outreader.ReadString('\n')
-			v, err := g.View("ubluout")
-			if err != nil {
-				// handle error
-			}
-			fmt.Fprint(v, text)
+			ubluout(g, text)
 		}
 	}()
 
 	// Deliver Ublu's stderr
 	go func() {
 		for {
-
 			text, _ := errreader.ReadString('\n')
-			v, err := g.View("ubluout")
-			if err != nil {
-				// handle error
-			}
-			fmt.Fprint(v, text)
-
+			ubluout(g, text)
 		}
 	}()
 
@@ -158,7 +113,8 @@ func main() {
 			// v.EditNewLine()
 			var l string
 			var err error
-			cx, cy := v.Cursor()
+			// cx, cy := v.Cursor()
+			_, cy := v.Cursor()
 			if l, err = v.Line(cy); err != nil {
 				l = ""
 			}
@@ -176,38 +132,11 @@ func main() {
 			v.MoveCursor(1, 0, false)
 		}
 	})
-	/*
-		if err := g.SetKeybinding("ubluin", gocui.KeyEnter, gocui.ModNone, func(g *gocui.Gui, v *gocui.View) error {
-			var l string
-			var err error
 
-			_, cy := v.Cursor()
-			if l, err = v.Line(cy); err != nil {
-				l = ""
-			}
-
-			w, werr := g.View("ubluout")
-			if werr != nil {
-				// handle error
-			}
-			fmt.Fprint(w, l)
-
-			io.WriteString(stdin, l+"\n")
-
-			return nil
-		}); err != nil {
-			log.Panicln(err)
-		}
-	*/
 	defer g.Close()
 
 	g.Cursor = true
 	g.SetManagerFunc(layout)
-	// ubluout(g, "GoUblu front end for Ublu")
-
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModAlt, quit); err != nil {
-		log.Panicln(err)
-	}
 
 	go func() {
 		if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
