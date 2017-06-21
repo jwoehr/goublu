43cc49ac54f44e5612539fb5c943f4eb29c44ebf
Jack Woehr <jwoehr@absolute-performance.com>
2017-06-21 13:36:21 -0600

    cleanup

--- reqmgr.go
+++ reqmgr.go
@@ -49,21 +49,6 @@ func NewReqManager(um *UbluManager, g *gocui.Gui, x int, y int, title string, bg
 		cy = cy
 
 		switch {
-		/*
-			case ch != 0 && mod == 0:
-				v.EditWrite(ch)
-			case key == gocui.KeySpace:
-				v.EditWrite(' ')
-			case key == gocui.KeyBackspace || key == gocui.KeyBackspace2:
-				v.EditDelete(true)
-			case key == gocui.KeyDelete:
-				v.EditDelete(false)
-			case key == gocui.KeyInsert:
-				v.Overwrite = !v.Overwrite
-			case key == gocui.KeyEnter:
-				rm.Ubluin(rm.G, v)
-				termbox.Interrupt() // for good luck
-		*/
 		case key == gocui.KeyArrowDown:
 			v.MoveCursor(0, 1, false)
 		case key == gocui.KeyArrowUp:
@@ -72,35 +57,12 @@ func NewReqManager(um *UbluManager, g *gocui.Gui, x int, y int, title string, bg
 			v.MoveCursor(-1, 0, false)
 		case key == gocui.KeyArrowRight:
 			v.MoveCursor(1, 0, false)
-			/*
-				case key == gocui.MouseWheelUp:
-					v.MoveCursor(0, -1, false)
-				case key == gocui.MouseWheelDown:
-					v.MoveCursor(0, 1, false)
-			*/
-			/*
-				case key == gocui.KeyCtrlA:
-					v.MoveCursor(0-cx, 0, false)
-				case key == gocui.KeyCtrlB:
-					v.MoveCursor(-1, 0, false)
-				case key == gocui.KeyCtrlE:
-					v.MoveCursor(len(text)-cx, 0, false)
-				case key == gocui.KeyCtrlF:
-					v.MoveCursor(1, 0, false)
-				case key == gocui.KeyCtrlK:
-					// this isn't quite correct but sorta works
-					for i := cy; i < gy; i++ {
-						v.EditDelete(false)
-					}
-				case key == gocui.KeyF4:
-					f, err := ioutil.TempFile(rm.Opts.SaveOutDir, "goublu.out.")
-					if err != nil {
-						log.Panicln(err)
-					}
-					rm.Ubluout(rm.G, "Saving output to "+f.Name()+"\n")
-					f.Write([]byte(rm.Hist.AllOut))
-					f.Close()
-			*/
+		/*
+		case key == gocui.MouseWheelUp:
+			v.MoveCursor(0, -1, false)
+			case key == gocui.MouseWheelDown:
+			v.MoveCursor(0, 1, false)
+		*/
 		case key == gocui.KeyF3:
 			rm.EndReq()
 		}
